da475061e82407a309f0ff7280e97a0ed2e59258
yokrh <y.oka.gml@gmail.com>
2018-04-15 05:12:17 +0000

    modify router for biography pagination

--- src/router/router.js
+++ src/router/router.js
@@ -2,9 +2,13 @@ import VueRouter from 'vue-router'
 import Top from '../pages/Top.vue'
 
 const routes = [
-  { path: '', name: 'Top', component: Top }
+  { path: '', name: 'Top', component: Top },
+  { path: '/page/:page', name: 'Page', component: Top }
 ];
 
 export default new VueRouter({
-    routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
